1574ba9d46dcb6c68c59243f2de3e0837b456b32
Manara-Ali <manaraali64@gmail.com>
2024-02-21 14:22:27 -0500

    Add additional settings in package.json

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
+const path = require("path");
 const express = require("express");
 const userRouter = require("./routers/userRoutes");
 
 const app = express();
 
 app.use("/api/v1/users", userRouter);
+
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...")
+  })
+}
 
 module.exports = app;
