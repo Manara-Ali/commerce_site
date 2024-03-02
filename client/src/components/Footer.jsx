239041fd112cb3897cd9a9ab76afee68d3064d2c
Manara-Ali <manaraali64@gmail.com>
2024-03-01 22:48:22 -0500

    Added copyright date

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 export const Footer = () => {
-    return(
-        <footer className="footer-container d-flex flex-column justify-content-center align-items-center">
-            <p>Built with <span id="heart-emoji">♥️</span> by</p>
-            <p>Copyright &copy; <span id="company-name">ColdFusion</span> Technology 2024</p>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer-container d-flex flex-column justify-content-center align-items-center">
+      <p>
+        Built with <span id="heart-emoji">♥️</span> by
+      </p>
+      <p>
+        Copyright &copy; <span id="company-name">ColdFusion</span> Technology
+      </p>
+    </footer>
+  );
+};
