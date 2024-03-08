c60c314db8d205cb3b795c2ef195ed4468ff23b9
Manara-Ali <manaraali64@gmail.com>
2024-03-08 10:05:24 -0500

    Round up the temperature

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,7 +1,7 @@
 import tempIcon from "../assets/temp.png";
 
 export const Weather = ({ location, weather }) => {
-  const tempFarenheit = Math.ceil(weather.main?.temp - 273.15) * (9 / 5) + 32;
+  const tempFarenheit = (Math.ceil(weather.main?.temp - 273.15) * (9 / 5) + 32)?.toFixed(0);
   return (
     <div className="d-flex flex-column align-items-center mr-3">
       <span>
