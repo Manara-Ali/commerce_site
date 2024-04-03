94ef272fd404280e4850ec50342f873aa310ccdd
Manara-Ali <manaraali64@gmail.com>
2024-04-03 16:39:20 -0400

    Fix min and max price in prod

--- client/src/utils/useMinMax.js
+++ client/src/utils/useMinMax.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useMinMax = (arr) => {
 
-  const priceArr = arr?.map((element) => {
+  // const priceArr = arr?.map((element) => {
+  //   return element.price;
+  // });
+
+  let priceArr = [];
+
+  useEffect(() => {
+    priceArr = arr?.map((element) => {
     return element.price;
   });
+  }, [arr]);
 
   const min = Math.min(...priceArr);
   const max = Math.max(...priceArr);
