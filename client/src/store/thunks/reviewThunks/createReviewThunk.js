f8e42aa9cab7fa34c3c5723e361d5a2ffb55a932
Manara-Ali <manaraali64@gmail.com>
2024-04-07 17:15:22 -0400

    Update url to prod

--- client/src/store/thunks/reviewThunks/createReviewThunk.js
+++ client/src/store/thunks/reviewThunks/createReviewThunk.js
@@ -2,11 +2,11 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const createReviewThunk = createAsyncThunk("review/create", async (data, {rejectWithValue}) => {
-    console.log(data);
+    // console.log(data);
     try {
         const response = await axios({
-            url: `/api/v1/meals/${data.slug}/reviews`,
-            // url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data.slug}/reviews`,
+            // url: `/api/v1/meals/${data.slug}/reviews`,
+            url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data.slug}/reviews`,
             method: "POST",
             data,
         },
@@ -14,7 +14,7 @@ export const createReviewThunk = createAsyncThunk("review/create", async (data, 
             withCredentials: true,
         });
 
-        console.log(response.data);
+        // console.log(response.data);
 
         return response.data;
     } catch (error) {
