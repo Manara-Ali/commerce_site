2f1f57f3a09a85c1bdb032c93ade8f796998b6a2
Manara-Ali <manaraali64@gmail.com>
2024-06-08 10:01:29 -0400

    Update summary and checkout page

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -180,7 +180,7 @@ export const Checkout = ({ children }) => {
                 <p>
                   {item.qty} x ${item.price} ={" "}
                   <span className="font-weight-bold">
-                    ${item.qty * item.price}
+                    $({item.qty * item.price}).toFixed(2)
                   </span>
                 </p>
               </div>
