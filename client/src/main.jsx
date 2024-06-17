44fd8da22867e1933488b1a9a0e15d40a82d87be
Manara-Ali <manaraali64@gmail.com>
2024-06-17 12:42:02 -0400

    Disable react devtools in production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,7 @@ import { App } from "./App.jsx";
 import { store } from "./store/index.js";
 import { CustomProvider } from "./context/ModalContext.jsx";
 import { CustomQuantityProvider } from "./context/QuantityContext.jsx";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 const stripeLoad = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
@@ -22,7 +23,7 @@ const options = {
   amount: 1099,
 };
 
-console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
