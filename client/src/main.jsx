7319f729999c90f5bf69081b0322dbcd640d0c08
Manara-Ali <manaraali64@gmail.com>
2024-06-27 23:13:19 -0400

    Adding custom price and size provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import {HelmetProvider} from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 import { persistor } from "./store/index.js";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -13,7 +13,9 @@ import { App } from "./App.jsx";
 import { store } from "./store/index.js";
 import { CustomProvider } from "./context/ModalContext.jsx";
 import { CustomQuantityProvider } from "./context/QuantityContext.jsx";
-import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+import { CustomSizeProvider } from "./context/SizeContext.jsx";
+import { CustomPriceProvider } from "./context/PriceContext.jsx";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 const stripeLoad = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
@@ -23,20 +25,24 @@ const options = {
   amount: 1099,
 };
 
-if(process.env.NODE_ENV === "production") disableReactDevTools();
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <CustomProvider>
-          <CustomQuantityProvider>
-            <Elements stripe={stripeLoad} options={options}>
-              <HelmetProvider>
-              <App />
-              </HelmetProvider>
-            </Elements>
-          </CustomQuantityProvider>
+          <CustomPriceProvider>
+            <CustomSizeProvider>
+              <CustomQuantityProvider>
+                <Elements stripe={stripeLoad} options={options}>
+                  <HelmetProvider>
+                    <App />
+                  </HelmetProvider>
+                </Elements>
+              </CustomQuantityProvider>
+            </CustomSizeProvider>
+          </CustomPriceProvider>
         </CustomProvider>
       </PersistGate>
     </Provider>
