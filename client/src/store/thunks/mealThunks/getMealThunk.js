2d3d8e96ce8af11b99b5c1ac396990d4dd50119d
Manara-Ali <manaraali64@gmail.com>
2024-07-24 12:38:43 -0400

    Revert to dev url

--- client/src/store/thunks/mealThunks/getMealThunk.js
+++ client/src/store/thunks/mealThunks/getMealThunk.js
@@ -5,8 +5,8 @@ export const getMealThunk = createAsyncThunk("meal/get-one", async (data, {rejec
     console.log(data);
     try {
         const response = await axios({
-            // url: `/api/v1/meals/${data}`,
-            url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data}`,
+            url: `/api/v1/meals/${data}`,
+            // url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data}`,
             method: "GET",
         },
         {
