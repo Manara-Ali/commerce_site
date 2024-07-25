53eb8a0779ba4d99f817681aa1682f09eae5a7f8
Manara-Ali <manaraali64@gmail.com>
2024-07-24 21:14:57 -0400

    Update to prod url for demo

--- client/src/store/thunks/paymentThunks/verifyPaymentThunk.js
+++ client/src/store/thunks/paymentThunks/verifyPaymentThunk.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 export const verifyPaymentThunk = createAsyncThunk("verify/payment", async (data, {rejectWithValue}) => {
     try {
         const response = await axios({
-            url: `/api/v1/stripe/verify-payment/${data.paymentId}`,
-            // url: `https://commerce-site-igmb.onrender.com/api/v1/stripe/verify-payment/${data.paymentId}`,
+            // url: `/api/v1/stripe/verify-payment/${data.paymentId}`,
+            url: `https://commerce-site-igmb.onrender.com/api/v1/stripe/verify-payment/${data.paymentId}`,
             method: "POST",
         },
         {
             withCredentials: true,
         });
-        
+
         return response.data;
     } catch (error) {
         return rejectWithValue(error.response?.data);
--- client/src/store/thunks/reviewThunks/createReviewThunk.js
+++ client/src/store/thunks/reviewThunks/createReviewThunk.js
@@ -5,8 +5,8 @@ export const createReviewThunk = createAsyncThunk("review/create", async (data, 
     // console.log(data);
     try {
         const response = await axios({
-            url: `/api/v1/meals/${data.slug}/reviews`,
-            // url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data.slug}/reviews`,
+            // url: `/api/v1/meals/${data.slug}/reviews`,
+            url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data.slug}/reviews`,
             method: "POST",
             data,
         },
