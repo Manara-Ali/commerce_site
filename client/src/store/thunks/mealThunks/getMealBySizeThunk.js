53e91368d2d9704ea21bf5d3ef7196dc10825e17
Manara-Ali <manaraali64@gmail.com>
2024-07-29 20:04:39 -0400

    Update getMealBySize thunk

--- client/src/store/thunks/mealThunks/getMealBySizeThunk.js
+++ client/src/store/thunks/mealThunks/getMealBySizeThunk.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 
 export const getMealBySizeThunk = createAsyncThunk("meal/get-one-by-size", async (obj, {rejectWithValue}) => {
     try {
-        const response = await axios({
+        const response = await axios(
+          {
             // url: `/api/v1/meals/${obj.data}?size=${obj.size}`,
-            url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${data}`,
+            url: `https://commerce-site-igmb.onrender.com/api/v1/meals/${obj.data}?size=${obj.size}`,
             method: "GET",
-        },
-        {
+          },
+          {
             withCredentials: true,
-        });
+          }
+        );
         return response.data;
     } catch (error) {
         return rejectWithValue(error.response?.data);
