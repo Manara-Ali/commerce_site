8ca78123c8841c9b8c205065a28a45eae54acbaf
Manara-Ali <manaraali64@gmail.com>
2024-08-15 21:10:01 -0400

    Update the contact page with social icons

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { AiFillTikTok } from "react-icons/ai";
 
 export const Contact = ({ children }) => {
   return (
@@ -12,120 +15,152 @@ export const Contact = ({ children }) => {
         />
         <link rel="canonical" href="/contact" />
       </Helmet>
-    <div>
-      <div className="d-flex justify-content-between">{children}</div>
-      <Link style={{ width: "15rem" }} to={"/"}>
-        <button
-          className="btn m-3 p-3 d-flex align-items-center justify-content-center"
-          id="back-btn"
+      <div>
+        <div className="d-flex justify-content-between">{children}</div>
+        <Link style={{ width: "15rem" }} to={"/"}>
+          <button
+            className="btn m-3 p-3 d-flex align-items-center justify-content-center"
+            id="back-btn"
+          >
+            {" "}
+            <i className="fa fa-arrow-left fa-2x mr-3" aria-hidden="true"></i>
+            BACK
+          </button>
+        </Link>
+        <h1 className="display-3 mt-5 mb-4 text-center">Contact Us</h1>
+        <p
+          className="px-4"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
         >
-          {" "}
-          <i className="fa fa-arrow-left fa-2x mr-3" aria-hidden="true"></i>BACK
-        </button>
-      </Link>
-      <h1 className="display-3 mt-5 mb-4 text-center">Contact Us</h1>
-      <p
-        className="px-4"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Thank you for considering <span className="business-name">Mimi's Kitchen & Grill</span> for your catering needs
-        and online orders! We're excited to help make your event unforgettable.
-      </p>
+          Thank you for considering{" "}
+          <span className="business-name">Mimi's Kitchen & Grill</span> for your
+          catering needs and online orders! We're excited to help make your
+          event unforgettable.
+        </p>
 
-      <p
-        className="px-4"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        We're here to help and assist you with any questions or inquiries you
-        may have. Please feel free to reach out to us using the contact
-        information below:
-      </p>
+        <p
+          className="px-4"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          We're here to help and assist you with any questions or inquiries you
+          may have. Please feel free to reach out to us using the contact
+          information below:
+        </p>
 
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Address:
-        <p className="lead my-0">[Your Business Address]</p>
-        <p className="lead my-0">[City, State, ZIP Code]</p>
-        <p className="lead my-0">[Country]</p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Phone:
-        <p className="lead my-0">[Your Phone Number]</p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Email:
-        <p className="lead my-0">[Your Email Address]</p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Operating Hours:
-        <p className="lead my-0">Monday - Friday: 9:00 AM - 6:00 PM</p>
-        <p className="lead my-0">Saturday: 10:00 AM - 4:00 PM</p>
-        <p className="lead my-0"> Sunday: Closed</p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Connect With Us:
-        <p className="lead my-0">Facebook</p>
-        <p className="lead my-0">Instagram</p>
-        <p className="lead my-0">LinkedIn</p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Event Inquiries:
-        <p className="lead my-0">
-          For event inquiries, consultations, and catering quotes, please email
-          us at [Your Event Email Address].
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Address:
+          <p className="lead my-0">[Your Business Address]</p>
+          <p className="lead my-0">[City, State, ZIP Code]</p>
+          <p className="lead my-0">[Country]</p>
         </p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        General Inquiries:
-        <p className="lead my-0">
-          For general questions and information, please email us at [Your
-          General Email Address].
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Phone:
+          <p className="lead my-0">[Your Phone Number]</p>
         </p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Feedback:
-        <p className="lead my-0">
-          We appreciate your feedback! Let us know how we did by emailing [Your
-          Feedback Email Address].
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Email:
+          <p className="lead my-0">
+            <a href=""></a>
+          </p>
         </p>
-      </p>
-      <p
-        className="px-4 lead font-weight-bold"
-        style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
-      >
-        Customer Support:
-        <p className="lead my-0">
-          For customer support, please email us at [Your Customer Support Email
-          Address] or call us at [Your Customer Support Phone Number].
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Operating Hours:
+          <p className="lead my-0">Monday - Friday: 9:00 AM - 6:00 PM</p>
+          <p className="lead my-0">Saturday: 10:00 AM - 4:00 PM</p>
+          <p className="lead my-0"> Sunday: Closed</p>
         </p>
-        <p className="lead mt-5">
-          At <span className="business-name">Mimi's Kitchen & Grill</span>, we always look forward to creating an exceptional culinary experience for you and your loved ones!
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Connect With Us:
+          <p className="lead my-0">
+            <a
+              href="https://www.facebook.com/profile.php?id=61563933896693&mibextid=LQQJ4d"
+              target="_blank"
+            >
+              <FaFacebookSquare size={25} color="#1877F2" />
+              Facebook
+            </a>
+          </p>
+          <p className="lead my-0">
+            <a
+              href="https://www.instagram.com/mimis_kitchen_grill/"
+              target="_blank"
+            >
+              <FaSquareInstagram size={25} color="#f09433" />
+              Instagram
+            </a>
+          </p>
+          <p className="lead my-0">
+            <a
+              className=".instagram"
+              href="https://www.tiktok.com/@kitchen_and_grill"
+              target="_blank"
+            >
+              <AiFillTikTok size={25} color="#fe2858" />
+              Tik Tok
+            </a>
+          </p>
         </p>
-      </p>
-    </div>
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Event Inquiries:
+          <p className="lead my-0">
+            For event inquiries, consultations, and catering quotes, please
+            email us at [Your Event Email Address].
+          </p>
+        </p>
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          General Inquiries:
+          <p className="lead my-0">
+            For general questions and information, please email us at [Your
+            General Email Address].
+          </p>
+        </p>
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Feedback:
+          <p className="lead my-0">
+            We appreciate your feedback! Let us know how we did by emailing
+            [Your Feedback Email Address].
+          </p>
+        </p>
+        <p
+          className="px-4 lead font-weight-bold"
+          style={{ fontSize: "1.5rem", fontWeight: "400", color: "#262626" }}
+        >
+          Customer Support:
+          <p className="lead my-0">
+            For customer support, please email us at [Your Customer Support
+            Email Address] or call us at [Your Customer Support Phone Number].
+          </p>
+          <p className="lead mt-5">
+            At <span className="business-name">Mimi's Kitchen & Grill</span>, we
+            always look forward to creating an exceptional culinary experience
+            for you and your loved ones!
+          </p>
+        </p>
+      </div>
     </>
   );
 };
